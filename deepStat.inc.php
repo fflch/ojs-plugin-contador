74b9fb2b845e23f7cc7c9a1ee7d967fdcfcea6d0
Daniel Silva Ferreira <danielsilva@usp.br>
2023-10-24 08:52:19 -0300

    obtendo a lista de revistas

--- deepStat.inc.php
+++ deepStat.inc.php
@@ -9,17 +9,60 @@
 import('lib.pkp.classes.plugins.BlockPlugin');
 
 class deepStat extends BlockPlugin {
+	//variaveis do banco de dados
+    private $databaseHost;
+    private $databaseName;
+    private $databaseUsername;
+    private $databasePassword;
+
 
 	public function getContents($templateMgr, $request = null)
     {
-        $templateMgr->assign([
-          'madeByText' => 'Made with ❤ by the Public Knowledge Project',
-        ]);
 
-        return parent::getContents($templateMgr, $request);
+		$configFile = 'config.inc.php';
+
+        if (file_exists($configFile)) {
+            $config = parse_ini_file($configFile, true);
+
+            if (isset($config['database'])) {
+                $this->databaseHost = $config['database']['host'];
+                $this->databaseName = $config['database']['name'];
+                $this->databaseUsername = $config['database']['username'];
+                $this->databasePassword = $config['database']['password'];
+            }
+        }
+
+		
+        $revistas = $this->obterDados(); // Chama a função para obter a lista de revistas.
+    
+    $templateMgr->assign([
+        'madeByText' => 'Made with ❤ by the Public Knowledge Project',
+        'revistas' => implode(', ', $revistas), // Converte a lista de revistas em uma string
+    ]);
+    
+    return parent::getContents($templateMgr, $request);
     }
 
-
+	public function obterDados() {
+        try {
+            $pdo = new PDO("mysql:host={$this->databaseHost};dbname={$this->databaseName}", $this->databaseUsername, $this->databasePassword);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+            // Agora você pode fazer uma consulta SQL para obter a lista de revistas.
+            $query = "SELECT path FROM journals"; // Altere a consulta conforme necessário.
+            $stmt = $pdo->query($query);
+            $revistas = $stmt->fetchAll(PDO::FETCH_COLUMN, 0);
+    
+            // Verifica se há resultados
+            if (!empty($revistas)) {
+                return $revistas;
+            } else {
+                return "Nenhuma revista encontrada no banco de dados.";
+            }
+        } catch (PDOException $e) {
+            return "Erro ao conectar ao banco de dados: " . $e->getMessage();
+        }
+    }
 
 	/**
 	 * Install default settings on system install.
--- templates/block.tpl
+++ templates/block.tpl
@@ -2,11 +2,15 @@
  * plugins/blocks/deepStat/block.tpl
  *}
  <div class="pkp_block block_madeBy">
- {$madeByText|escape}
-
-
-
-
+ {$madeByText|escape}<br><br>
+ {$madeByText2|escape}<br><br>
+ Lista de revistas: {$revistas} <br><br>
+ 
+    Quantidade de fascículos: {$fasciculos} <br>
+    Quantidade de artigos: {$artigos} <br>
+    Quantidade de downloads: {$downloads} <br>
+    Quantidade de acessos: {$madeByText2} <br>
+	Teste: {$obterDados|escape}
 
 
 
